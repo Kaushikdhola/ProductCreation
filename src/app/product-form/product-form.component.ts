94fcf6ce6abc21e4f833d71d0b4d4ed1da1eb7f4
Kaushikdhola <kaushikdhola02@gmail.com>
2024-02-03 19:31:48 -0400

    update code

--- src/app/product-form/product-form.component.ts
+++ src/app/product-form/product-form.component.ts
@@ -1,4 +1,5 @@
-import { Component, NgModule } from '@angular/core';
+import { Component,ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-product-form',
@@ -8,12 +9,28 @@ import { Component, NgModule } from '@angular/core';
 
 export class ProductFormComponent {
   productData: any = {};
-  onSubmit(){
+  selectedFiles: File[] = [];
+
+
+  @ViewChild('productForm', { static: false }) productForm!: NgForm;
+
+  onSubmit(productForm: NgForm){
     console.log(this.productData)
+    this.productForm.resetForm();
   }
 
   onFileSelected(event: any) {
-    const file = event.target.files[0];
-    console.log(file)
+    const inputElement: HTMLInputElement = event.target;
+    if (inputElement.files) {
+      for (let i = 0; i < inputElement.files.length; i++) {
+        this.selectedFiles.push(inputElement.files[i]);
+      }
+    }
+    console.log(this.selectedFiles)
+
+    const fileInput: HTMLInputElement | null = document.getElementById('myFile') as HTMLInputElement;
+    if (fileInput) {
+      fileInput.value = '';
+    }
   }
 }
